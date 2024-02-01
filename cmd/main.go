5859580f2aaad40958a6e6075fd3d766d25cffb2
Marcus Martins <mbodock@gmail.com>
2024-02-01 17:56:12 -0300

    add instrumentation to golang service
    Now the service golang is proper instrumented with metrics and traces.

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/propagation"
+	sdkMetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/trace"
+
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
@@ -31,13 +42,62 @@ func main() {
 	}()
 
 	r := gin.Default()
-	r.Use(otelgin.Middleware("my-server"))
+	r.Use(otelgin.Middleware("Golang service middleware"))
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+	r.POST("/sum", func(c *gin.Context) {
+		otel.GetTextMapPropagator().Inject(c.Request.Context(), propagation.HeaderCarrier(c.Request.Header))
+		a, err := strconv.Atoi(c.PostForm("a"))
+		b, err := strconv.Atoi(c.PostForm("b"))
+		if err != nil {
+			c.JSON(400, gin.H{"msg": err})
+			return
+		}
+
+		result := fetchSumResult(c.Request.Context(), a, b)
+		c.JSON(http.StatusOK, gin.H{
+			"result": fmt.Sprintf("%d", result),
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func fetchSumResult(ctx context.Context, a, b int) int {
+	traceID := "fetchSum"
+	tracer := otel.GetTracerProvider().Tracer("")
+	ctx, span := tracer.Start(ctx, traceID,
+		trace.WithAttributes(attribute.String("args", fmt.Sprintf("a=%d,b=%d", a, b))),
+	)
+	defer span.End()
+
+	url := fmt.Sprintf("http://python:8081/sum/%d/%d", a, b)
+	client := &http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
+
+	tCtx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	req, _ := http.NewRequestWithContext(tCtx, http.MethodGet, url, nil)
+	resp, _ := client.Do(req)
+
+	data, _ := io.ReadAll(resp.Body)
+	sum, _ := strconv.Atoi(string(data))
+	span.AddEvent("response code", trace.WithAttributes(attribute.Int("code", resp.StatusCode)))
+
+	var meter = otel.Meter(traceID)
+	apiCounter, err := meter.Int64Counter(
+		"sumApiUses",
+		metric.WithDescription("Number of times we used the sum api."),
+		metric.WithUnit("{call}"),
+	)
+	apiCounter.Add(tCtx, 1)
+	if err != nil {
+		panic(err)
+	}
+	return sum
 }
 
 func initTracer() (*sdktrace.TracerProvider, error) {
@@ -66,7 +126,18 @@ func initTracer() (*sdktrace.TracerProvider, error) {
 		sdktrace.WithBatcher(exporter),
 		sdktrace.WithResource(res),
 	)
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
+	// Set up metrics provider prometheus.
+	// DefaultRegisterer is used by default
+	// so that metrics are available via promhttp.Handler.
+	mexporter, err := prometheus.New()
+	if err != nil {
+		panic(err)
+	}
+	pprovider := sdkMetric.NewMeterProvider(sdkMetric.WithReader(mexporter))
+	otel.SetMeterProvider(pprovider)                      // Sets global metrics provider
+	otel.SetTracerProvider(tp)                            // Sets global tracer provider
+	otel.SetTextMapPropagator(propagation.TraceContext{}) // Sets global propagator provider
+
 	return tp, nil
 }
--- go.mod
+++ go.mod
@@ -4,17 +4,24 @@ go 1.21.6
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/prometheus/client_golang v1.18.0
 	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.47.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.47.0
 	go.opentelemetry.io/otel v1.22.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.22.0
-	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.22.0
+	go.opentelemetry.io/otel/exporters/prometheus v0.45.0
 	go.opentelemetry.io/otel/sdk v1.22.0
+	go.opentelemetry.io/otel/sdk/metric v1.22.0
+	go.opentelemetry.io/otel/trace v1.22.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
@@ -29,14 +36,17 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.22.0 // indirect
 	go.opentelemetry.io/otel/metric v1.22.0 // indirect
-	go.opentelemetry.io/otel/trace v1.22.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
